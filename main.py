a07cfc6a207341fd41376d32bf5c2a861db22c40
democodingal <mayurirastogi0@gmail.com>
2024-10-22 23:16:48 +0530

    done

---
+++ main.py
@@ -0,0 +1,29 @@
+#assigning different variables
+name="codingal"
+age=10
+is_student=True
+weight=32
+
+#printing different variables and their datatypes
+print("name:  ", name)
+print("Datatype of name is:  ",type(name))
+
+print("age:  ",age)
+print("Datatype of age is:  ",type (age))
+
+print("is student:  ",is_student)
+print("Datatype of is_student is:  ",type(is_student))
+
+print("weight:  ",weight)
+print("Datatype of weight is:  ",type(weight))
+
+#typecasting to convert the datatypes of variables
+print("\n After Type Casting.....\n")
+
+age=str(age)
+print(age)
+print("Datatype of age is", type(age))
+
+weight=int(weight)
+print(weight)
+print("Datatype of weight is",type(weight))
